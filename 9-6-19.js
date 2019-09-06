f3a686bba4453bbdafc96a8177806ec8ff7471cf
Nick <nick.drishinski@rockfordcorp.com>
2019-09-06 09:23:07 -0700

    second challenge

--- 9-6-19.js
+++ 9-6-19.js
@@ -17,3 +17,19 @@ function spinWords(str){
     return newArr.join(' ')
   }
   
+
+//   You are given an array (which will have a length of at least 3, 
+//     but could be very large) containing integers. The array is either
+//     entirely comprised of odd integers or entirely comprised of even 
+//     integers except for a single integer N. Write a method that takes
+//     the array as an argument and returns this "outlier" N.
+
+function findOutlier(integers){
+    let odd = [];
+    let even = [];
+    integers.map((num) => {
+      num % 2 == 0 ? even.push(num) : odd.push(num)
+    })
+    
+    return odd.length > even.length ? even[0] : odd[0]
+  }
