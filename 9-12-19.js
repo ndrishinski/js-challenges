e47402d2febb61aa45744343e0b55ad582209b05
Nick <nick.drishinski@rockfordcorp.com>
2019-09-12 11:53:56 -0700

    get middle of string

--- 9-12-19.js
+++ 9-12-19.js
@@ -16,3 +16,32 @@ function divisors(integer) {
     
     return answ[0] ? answ : `${integer} is prime`
   };
+
+//   You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
+
+function getMiddle(s) {
+    var isEven = s.length % 2 == 0 ? 1 : 0
+    
+    if (isEven) {
+      return s.slice(s.length / 2 - 1, s.length / 2 + 1) 
+    } else {
+      return s.slice(s.length / 2, s.length / 2 + 1)
+    }
+  }
