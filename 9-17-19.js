8472f95e3810b6c5f524336eb8ae5420f5441892
Nick <nick.drishinski@rockfordcorp.com>
2019-09-17 12:44:11 -0700

    sum array except min and max

--- 9-17-19.js
+++ 9-17-19.js
@@ -7,9 +7,25 @@
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 
 var uniqueInOrder=function(iterable){
-    let final = [];
-    for (let i = 0; i < iterable.length; i++) {
-      iterable[i] != iterable[i - 1] ? final.push(iterable[i]) : null
-    }
-    return final;
+    // let final = [];
+    // for (let i = 0; i < iterable.length; i++) {
+    //   iterable[i] != iterable[i - 1] ? final.push(iterable[i]) : null
+    // }
+    // return final;
+    console.log([...iterable]);
+    return [...iterable].filter((a, i) => a !== iterable[i-1])
+  }
+
+  // Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+  // (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+  
+  // Example:
+  
+  // { 6, 2, 1, 8, 10 } => 16
+  // { 1, 1, 11, 2, 3 } => 6
+  
+  // If array is empty, null or None, or if only 1 Element exists, return 0.
+
+  function sumArray(array) {
+    return array && array.length > 2 ? array.sort((a, b) => a - b).slice(1, array.length - 1).reduce((tot, item) => tot += item ) : 0;
   }
