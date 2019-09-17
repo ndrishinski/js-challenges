3a732b5bfe3374351d08c6572d772435fcbb33a3
Nick <nick.drishinski@rockfordcorp.com>
2019-09-17 15:52:43 -0700

    compare christmas lists

--- 9-17-19.js
+++ 9-17-19.js
@@ -51,3 +51,18 @@ function createPhoneNumber(numbers){
 function squareDigits(num){
   return Number(`${num}`.split('').map(item => item * item).join(''))
 }
+
+// Christmas is comming, and Santa has a long list to go through, 
+// to find who deserves presents for the big day. Go through a list of 
+// children, and return a list containing every child who appeared on 
+// Santas list. Do not add any child more than once. Output should be sorted
+// . For java, use Lists.
+
+// Comparison should be case sensitive and the returned list should contain 
+// only one copy of each name. "Sam" and "sam" is allowed, but "sAm" and "sAm" is not.
+
+function findChildren(santasList, children) {
+  return santasList.filter((item, ind) => {
+    return children.indexOf(item) != -1 && ind == santasList.lastIndexOf(item)
+  }).sort();
+}
