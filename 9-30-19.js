4c6ca10e947b9fc242106fc205783438b9483005
Nick <nick.drishinski@rockfordcorp.com>
2019-09-30 11:11:09 -0700

    name list

--- 9-30-19.js
+++ 9-30-19.js
@@ -113,3 +113,34 @@ function dontGiveMeFive(start, end) {
   }
   return num;
 }
+
+// Given: an array containing hashes of names
+
+// Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+// Example:
+
+// list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// // returns 'Bart, Lisa & Maggie'
+
+// list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// // returns 'Bart & Lisa'
+
+// list([ {name: 'Bart'} ])
+// // returns 'Bart'
+
+// list([])
+// // returns ''
+// Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
+
+function list(names){
+  let nameList = '';
+  if (!names.length) return ''
+  else if (names.length == 1) return names[0].name
+  
+  for (let i of names.slice(0, names.length -2)) {
+    nameList += i.name + ', '
+  }
+
+   return `${nameList}${names[names.length -2].name} & ${names[names.length -1].name}`
+}
