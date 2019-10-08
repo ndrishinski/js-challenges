64cf21e81404a7152ae5d3cef9bff380a2040a68
Nick <nick.drishinski@rockfordcorp.com>
2019-10-08 11:20:29 -0700

    bouncing ball

---
+++ 10-8-19.js
@@ -0,0 +1,64 @@
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+const binaryArrayToNumber = arr => {
+    let newArr = arr.reverse()
+    let int = 1
+    let tot = 0;
+    newArr.map((item) => {
+      if (item) {
+        tot += int
+        int *= 2
+      } else {
+        int *= 2
+      }
+    })
+    
+    return tot
+  }
+
+//   A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+
+// He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+// His mother looks out of a window 1.5 meters from the ground.
+
+// How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+// Three conditions must be met for a valid experiment:
+// Float parameter "h" in meters must be greater than 0
+// Float parameter "bounce" must be greater than 0 and less than 1
+// Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+// Note:
+// The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+
+// Example:
+// - h = 3, bounce = 0.66, window = 1.5, result is 3
+
+// - h = 3, bounce = 1, window = 1.5, result is -1 
+
+// (Condition 2) not fulfilled).
+
+function bouncingBall(h,  bounce,  window) {
+  let tot = 0;
+  while (h > window) {
+    h = (h * bounce)
+    h > window ? tot += 2 : tot += 1
+  }
+  return tot ? tot : -1
+}
