995cb566e20aac9f60e1988527a3e2802fb8854b
Nick <nick.drishinski@rockfordcorp.com>
2019-10-11 13:45:06 -0700

    finding right century

--- 10-11-19.js
+++ 10-11-19.js
@@ -100,3 +100,25 @@ function isTriangle(a,b,c) {
   function test(arr) {
     return arr[0] + arr[1] > arr[2] ? true : false
   }
+
+//   Write function avg which calculates average of numbers in given list.
+
+function find_average(array) {
+    return array.reduce((tot, item) => tot+= item) / array.length
+  }
+
+//   Introduction
+// The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+// Task :
+// Given a year, return the century it is in.
+
+// Input , Output Examples ::
+// centuryFromYear(1705)  returns (18)
+// centuryFromYear(1900)  returns (19)
+// centuryFromYear(1601)  returns (17)
+// centuryFromYear(2000)  returns (20)
+
+function century(year) {
+    return year / 100 % 1 == 0 ? year /100 : Math.floor(year / 100) + 1
+  }
