72a6f8725991d5c4a80c6586f08665ff6a220e07
Nick <nick.drishinski@rockfordcorp.com>
2019-10-14 10:29:05 -0700

    repeatSTr

---
+++ 9-14-19.js
@@ -0,0 +1,39 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return args.reduce((prev, next) => prev > next ? prev = next : prev)
+    }
+  }
+
+//   It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+// Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+// If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+// Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS and Java) or "$" (C#, C++, Ruby, Clojure, Elixir, PHP and Python, Haskell).
+
+
+function bonusTime(salary, bonus) {
+    return bonus ? `£${salary * 10}` : `£${salary}`
+  }
+
+//   Write a function called repeatString which repeats the given String src exactly count times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+    let newS = s;
+    for (let i = 1; i < n; i++) {
+      newS += s
+    }
+    return newS
+  }
