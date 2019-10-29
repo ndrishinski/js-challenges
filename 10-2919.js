b7923f98cbde58017d7ce90a74e4ace664bd0abd
Nick <nick.drishinski@rockfordcorp.com>
2019-10-29 10:07:17 -0700

    string to array

--- 10-2919.js
+++ 10-2919.js
@@ -7,3 +7,13 @@
 function friend(friends){
   return friends.filter(item => item.length == 4)
 }
+
+// Write a function to split a string and convert it into an array of words. For example:
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string){
+  return string.split(' ');
+}
