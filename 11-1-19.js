6b0aa907466b0f4351d4419ea5bdba985b2c4861
Nick <nick.drishinski@rockfordcorp.com>
2019-11-01 10:30:20 -0700

    findMissingLetter

--- 11-1-19.js
+++ 11-1-19.js
@@ -46,3 +46,37 @@ function basicOp(operation, value1, value2) {
       return value1 / value2
   }
 }
+
+// #Find the missing letter
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+// ["a","b","c","d","f"] -> "e"
+// ["O","Q","R","S"] -> "P"
+// (Use the English alphabet with 26 letters!)
+
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+// I have also created other katas. Take a look if you enjoyed this kata!
+
+function findMissingLetter(array) {
+  let alpha = 'abcdefghijklmnopqrstuvwxyz';
+  let alphaArr = alpha.split('');
+  
+  let sub = alphaArr.indexOf(array[0].toLowerCase())
+  let ans = alphaArr.splice(sub, array.length)
+  
+  for ( let i = 0; i < ans.length; i++ ) {
+    if (ans[i] != array[i].toLowerCase()) {
+      return array[i - 1] == ans[i - 1] ? ans[i] : ans[i].toUpperCase()
+    }
+  }
+    
+}
