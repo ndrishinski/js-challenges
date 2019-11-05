02f6e542e96840774f1a536b91b461de09abdfbe
Nick <nick.drishinski@rockfordcorp.com>
2019-11-05 09:19:50 -0700

    several daily challenges

---
+++ 11-5-19.js
@@ -0,0 +1,125 @@
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+
+const rps = (p1, p2) => {
+  let arr = [p1, p2]
+  let obj = {}
+  arr.map((item, ind) => obj[item] = ind + 1)
+  if (p1 == p2) return 'Draw!'
+  if (arr.includes('rock') && arr.includes('paper')) return `Player ${obj['paper']} won!`
+  if (arr.includes('rock') && arr.includes('scissors')) return `Player ${obj['rock']} won!`
+  if (arr.includes('paper') && arr.includes('scissors')) return `Player ${obj['scissors']} won!`
+  
+};
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+  return a.includes(x)
+}
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+  return x.split(' ').join('')
+}
+
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+function litres(time) {
+  return Math.floor(time * .5)
+}
+
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+var countSheep = function (num){
+  let str = '';
+  for ( let i = 0; i < num; i++) {
+    str += `${i + 1} sheep...`
+  }
+  return str
+}
+
+// Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+// As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+// If a string contains all repeating characters, it should return an empty string ("") or None -- see sample tests.
+
+function firstNonRepeatingLetter(str) {
+  let s = str.toLowerCase()
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) == s.lastIndexOf(s[i])) return str[i]
+  }
+  return ''
+}
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+// Notes
+
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+function duplicateEncode(word){
+  let arr = word.toLowerCase().split('')
+  return arr.map(item => arr.indexOf(item) != arr.lastIndexOf(item) ? ')' : '(').join('')
+}
+
+// Enough is enough!
+// Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like this sessions, since the motive usually repeats. He isn't fond of seeing the Eiffel tower 40 times. He tells them that he will only sit during the session if they show the same motive at most N times. Luckily, Alice and Bob are able to encode the motive as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+
+// Task
+// Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+
+// Example
+//   deleteNth ([1,1,1,1],2) // return [1,1]
+
+//   deleteNth ([20,37,20,21],1) // return [20,37,21]
+
+function deleteNth(arr,n){
+  let obj = {};
+  
+  arr.map(item => {
+    if (obj[item] && obj[item] < n) obj[item] += 1 
+    else if (!obj[item]) obj[item] = 1
+  })
+  
+  return arr.filter(item => {
+    obj[item] -=1;
+    return obj[item] >= 0;
+  })
+  
+  
+  }
