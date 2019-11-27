55bba6688e6bb5b70beb43d808667bf1c28e1090
ndrishinski <ndrishinski@yahoo.com>
2019-11-27 08:08:28 -0700

    findSenior

--- 11-27-19.js
+++ 11-27-19.js
@@ -8,3 +8,33 @@ function twoSort(s) {
     let arr = s.sort()[0].split('')
     return arr.map((i, ind) => ind != arr.length -1 ? i += '***' : i).join('')
   }
+
+//   You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an array which includes the developer who is the oldest. In case of a tie, include all same-age senior developers listed in the same order as they appeared in the original input array.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//   { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+//   { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+//   { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ];
+// your function should return the following array:
+
+// [
+//   { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//   { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ]
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+
+function findSenior(list) {
+    let oldest = 0
+    let arr = list.map(i => {
+      if (i.age >= oldest) oldest = i.age
+    })
+    return list.filter(i => i.age == oldest)
+  }
