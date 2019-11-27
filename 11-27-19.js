592e01991efddc1740022f7c76c7b17497eb99d0
ndrishinski <ndrishinski@yahoo.com>
2019-11-27 13:53:17 -0700

    football score

--- 11-27-19.js
+++ 11-27-19.js
@@ -164,3 +164,28 @@ function arithmetic(a, b, operator){
     
     return obj[operator]
   }
+
+//   Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+// if x>y - 3 points
+// if x<y - 0 point
+// if x=y - 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function score(game) {
+    if (game[0] > game[2]) return 3
+    if (game[0] < game[2]) return 0
+    if (game[0] == game[2]) return 1
+  }
+  
+  function points(games) {
+    return games.map(i => score(i)).reduce((tot, i) => tot+=i, 0)
+  }
