80a8b16db55cef323021d594f9f09eeca1aa653b
ndrishinski <ndrishinski@yahoo.com>
2019-12-10 08:55:58 -0700

    fullName class

--- 12-10-19.js
+++ 12-10-19.js
@@ -26,3 +26,30 @@ function upArray(arr){
       }
       return !isNaN(num) ? [...String(num)].map(i => Number(i)) : null
     }
+
+//     The code provided is supposed return a person's Full Name given their first and last names.
+
+// But it's not working properly.
+
+// Notes
+// The first and/or last names are never null (None in Python), but may be empty.
+
+// Task
+// Fix the bug so we can all go home early.
+
+class Dinglemouse{
+
+    constructor( firstName, lastName ){
+      this.firstName = firstName.replace(/ /g, '')
+      this.lastName = lastName.replace(/ /g, '')
+    }
+    
+    getFullName(){
+      if (this.firstName && this.lastName) return this.firstName+" "+this.lastName
+      else if (this.firstName && !this.lastName) return this.firstName
+      else if (!this.firstName && this.lastName) return this.lastName
+      else return ''
+      
+    }
+    
+  }
