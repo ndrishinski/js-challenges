6bd824746ccb7dc2f54b3c829376dd82ad5bba51
ndrishinski <ndrishinski@yahoo.com>
2019-12-18 08:01:52 -0700

    repeater

--- 12-16-19.js
+++ 12-16-19.js
@@ -8,3 +8,14 @@
 
 const isLockNessMonster = (s) => /tree fiddy|3.50/.test(s)
 
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
