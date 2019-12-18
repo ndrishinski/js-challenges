1d4cd65f57dc3c0f9a83e5953ba08d50e472a2f6
ndrishinski <ndrishinski@yahoo.com>
2019-12-18 08:28:00 -0700

    find needle

--- 12-18-19.js
+++ 12-18-19.js
@@ -54,3 +54,17 @@ function titleCase(title, minorWords) {
       return str.split('').map((i, ind) => ind == 0 ? i.toUpperCase() : i.toLowerCase()).join('')
   }
   
+//   Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return "found the needle at position 5"
+
+function findNeedle(haystack) {
+    return `found the needle at position ${haystack.indexOf('needle')}`
+  }
